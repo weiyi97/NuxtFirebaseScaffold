728765fa391488a6d546d1f4136d48dc03b95229
Wei Yi <WYLeong@gamuda.com.my>
2022-04-06 04:36:25 +0800

    firestore modular SDK

--- firebase/init.js
+++ firebase/init.js
@@ -1,26 +1,77 @@
-import * as firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
-import 'firebase/database';
-import 'firebase/storage';
-import 'firebase/messaging';
+//migrated to version 9
+
+//version 9 compat imports
+import firebase from 'firebase/compat/app';
+// import 'firebase/compat/auth';
+// import 'firebase/compat/firestore';
+
+
+//version 9 modular style import
+import { initializeApp } from "firebase/app"
+import {getAuth, onAuthStateChanged,  setPersistence, signInWithEmailAndPassword, browserSessionPersistence} from 'firebase/auth';
+import { getFirestore, collection, getDoc, query, where, getDocs } from 'firebase/firestore';
+
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+
+// pre-version 9 import - DOES NOT WORK ANYMORE
+// import * as firebase from 'firebase/app';
+// import 'firebase/auth';
+// import 'firebase/firestore';
+// import 'firebase/database';
+// import 'firebase/storage';
+// import 'firebase/messaging';
+
+
 
 // Fill this in with your firebase details - instructions:
 // https://support.google.com/firebase/answer/7015592
 const config = {
-	apiKey: "",
-	authDomain: "",
-	projectId: "",
-	storageBucket: "",
-	messagingSenderId: "",
-	appId: "1",
-	measurementId: ""
+  apiKey: "AIzaSyDsAdMU_mrLV25NAyYJchNsHQQOkmVtJgU",
+  authDomain: "fir-project-714af.firebaseapp.com",
+  projectId: "fir-project-714af",
+  storageBucket: "fir-project-714af.appspot.com",
+  messagingSenderId: "313180306855",
+  appId: "1:313180306855:web:d854c478e3b5cf69e5ca82",
+  measurementId: "G-7G2CBL0F29"
 };
 
-!firebase.apps.length ? firebase.initializeApp(config) : '';
+//-----Initialize Firebase for version 9----------------------------------
 
-export const auth = firebase.auth();
-export const persistencetype = firebase.auth.Auth.Persistence.LOCAL;
+const app = initializeApp(config);
+// const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
+
+// firestore init services
+export const firestoreDB = getFirestore(app);
+
+// firestore collection ref
+export const userRef = collection(firestoreDB, "Users");
+
+//set auth persistence
+setPersistence(auth, browserSessionPersistence)
+  .then(() => {
+    // Existing and future Auth states are now persisted in the current
+    // session only. Closing the window would clear any existing state even
+    // if a user forgets to sign out.
+    // ...
+    // New sign-in will be persisted with session persistence.
+    return signInWithEmailAndPassword(auth, email, password);
+  })
+  .catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
+
+//-----Initialize Firebase for version 8----------------------------------
+
+// !firebase.apps.length ? firebase.initializeApp(config) : '';
+
+// export const auth = firebase.auth();
+// export const persistencetype = firebase.auth.Auth.Persistence.LOCAL;
 
 // Comment this out if not using local emulator
 // firebase.firestore().settings({
@@ -29,7 +80,7 @@ export const persistencetype = firebase.auth.Auth.Persistence.LOCAL;
 // });
 
 //Specify the link to firestoreDB collection that you will be using, feel free to change, add, delete.
-export const firestoreDB = firebase.firestore()
+// export const firestoreDB = firebase.firestore()
 
 // export const storage = firebase.app().storage('gs://YOUR_BUCKET_HERE');
 // export const messaging = firebase.messaging();
@@ -37,10 +88,10 @@ export const firestoreDB = firebase.firestore()
 // messaging.usePublicVapidKey(
 //   'BDYE2EYHdIp8qHjTKcJYPvO4PgaAH2pSruP55FOtNs5jWsgdeg7YK6OgJ0daSu21kN7aSzU19NRXRqC4bfITZYQ'
 // );
-export const GoogleProvider = new firebase.auth.GoogleAuthProvider()
-export const FacebookProvider = new firebase.auth.FacebookAuthProvider()
-export const LinkedInProvider = new firebase.auth.OAuthProvider('linkedin.com');
-LinkedInProvider.addScope('r_liteprofile');
-LinkedInProvider.addScope('r_emailaddress');
+// export const GoogleProvider = new firebase.auth.GoogleAuthProvider()
+// export const FacebookProvider = new firebase.auth.FacebookAuthProvider()
+// export const LinkedInProvider = new firebase.auth.OAuthProvider('linkedin.com');
+// LinkedInProvider.addScope('r_liteprofile');
+// LinkedInProvider.addScope('r_emailaddress');
 
 export default firebase;
---
+++ pages/intervention/index.vue
@@ -0,0 +1,92 @@
+<template>
+  <div>
+    <h1>Intervention Home Page</h1>
+    <h3>Firestore data parsing....</h3>
+    <h3 class="my-8">{{ displayNames }}</h3>
+    <h3 @click="createUser()" style="cursor: pointer">Click to add user</h3>
+  </div>
+</template>
+
+<script>
+import { firestoreDB, userRef } from "../../firebase/init.js";
+import {
+  query,
+  where,
+  getDocs,
+  getDoc,
+  doc,
+  collection,
+  setDoc,
+} from "firebase/firestore";
+
+export default {
+  name: "Intervention",
+
+  data() {
+    return {
+      uid: "H2s6rpQiMHNj4pvHVPkU",
+      displayNames: [],
+    };
+  },
+  // middleware: "authentication"
+
+  methods: {
+    createUser() {
+      //-----create a document
+
+      // init collection ref
+      const docRef = collection(firestoreDB, "Users");
+      setDoc(doc(docRef), {
+        displayName: "Sze Tyng",
+        isDHP: true,
+      }).then(() => {
+        this.initialize();
+      });
+    },
+
+    initialize() {
+      this.displayNames = [];
+      //-----using query for firebase modular SDK
+      const q = query(
+        collection(firestoreDB, "Users"),
+        where("isDHP", "==", true)
+      );
+
+      getDocs(q).then((querySnapshot) => {
+        querySnapshot.docs.forEach((doc) => {
+          console.log(doc.data().displayName);
+
+          this.displayNames.push(doc.data().displayName);
+        });
+      });
+
+      //-----reading a single docucment with firebase modular SDK
+      const docRef = doc(firestoreDB, "Users", this.uid);
+
+      getDoc(docRef).then((doc) => {
+        console.log("read data once");
+        let data = doc.data();
+        console.log(data.displayName);
+      });
+    },
+  },
+
+  created() {
+    this.initialize();
+    // old firestore function
+    // userRef
+    //   .collection("Users")
+    //   .doc(this.uid)
+    //   .get()
+    //   .then((doc) => {
+    //     this.displayName = doc.data().displayName;
+    //   })
+    //   .catch((e) => {
+    //     console.log(e);
+    //   });
+  },
+};
+</script>
+
+<style scoped>
+</style>
